4c9a9d721d75924bf21be4fd7099a44c16ce4486
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-02-28 17:07:14 -0700

    update some meta information

--- pydiso/__init__.py
+++ pydiso/__init__.py
@@ -1,4 +1,19 @@
-__version__ = "0.0.3"
 __author__ = "SimPEG Team"
 __license__ = "MIT"
 __copyright__ = "2021, SimPEG Developers, http://simpeg.xyz"
+
+from importlib.metadata import version, PackageNotFoundError
+
+# Version
+try:
+    # - Released versions just tags:       0.8.0
+    # - GitHub commits add .dev#+hash:     0.8.1.dev4+g2785721
+    # - Uncommitted changes add timestamp: 0.8.1.dev4+g2785721.d20191022
+    __version__ = version("discretize")
+except PackageNotFoundError:
+    # If it was not installed, then we don't know the version. We could throw a
+    # warning here, but this case *should* be rare. discretize should be
+    # installed properly!
+    from datetime import datetime
+
+    __version__ = "unknown-" + datetime.today().strftime("%Y%m%d")
